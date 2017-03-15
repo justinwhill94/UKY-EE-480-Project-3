635bf8af4910069ca80a78e69bb28f75a5b01b1f
nstpsword95 <nstpsword95@gmail.com>
2017-03-15 14:41:56 -0400

    Update sik.v

--- sik.v
+++ sik.v
@@ -4,42 +4,53 @@
 *   Justin Hill, Noah Pierre, Abdullah Alosail
 */
 
-// Basic Sizes
-`define WORD [15:0]
+// -----------------------------Size parameters----------------------------------
+
+//N.S.
+`define WORDSIZE [15:0]
 `define OPCODE [15:12]
-`define ARG [11:0]
+//`define DST [11:6]
+`define REGSIZE [255:0]
+`define SP [7:0]      //stack pointer, i point to one of 256 words in the MEM
+`define PC [15:0]     // i point to one of 16 bits in the addressed WORD
+`define TorF          // im a 1bit boolean that keeps track of conditionals (TRUE OR FALSE)
+`define PRE [3:0]     // like in P2 i alow for 16 bit imeds
+`define isPREld       // im a 1 bit bolean keeping track if PRE is loaded
+//N.S
 
-/*
-`define DEST 
-`define SRC 
-`define STATE 
-`define REGSIZE 
-`define MEMSIZE 
-*/
+// -----------------------------Opcode Values-------------------------------------
 
-// Opcode Values
-`define NoArg 4'd0
-`define Call 4'd1
-`define Get 4'd2
-`define JumpF 4'd3
-`define Jump 4'd4
-`define JumpT 4'd5
-`define Pop 4'd6
-`define Pre 4'd7
-`define Push 4'd8
-`define Put 4'd9
+'define NoArg 4'd0
+'define Call 4'd1
+'define Get 4'd2
+'define JumpF 4'd3
+'define Jump 4'd4
+'define JumpT 4'd5
+'define Pop 4'd6
+'define Pre 4'd7
+'define Push 4'd8
+'define Put 4'd9
 
-// No Arg Opcode
-`define Add 8'd1
-`define And 8'd2
-`define Dup 8'd4
-`define Load 8'd8
-`define Lt 8'd16
-`define Or 8'd32
-`define Ret 8'd64
-`define Store 8'd128
-`define Sub 8'd256
-`define Sys 8'd512
-`define Test 8'd1024
-`define Xor 8'd2048
+// ----------------------------No Arg Opcode---------------------------------------
+
+'define Add 8'd1
+'define And 8'd2
+'define Dup 8'd4
+'define Load 8'd8
+'define Lt 8'd16
+'define Or 8'd32
+'define Ret 8'd64
+'define Store 8'd128
+'define Sub 8'd256
+'define Sys 8'd512
+'define Test 8'd1024
+'define Xor 8'd2048
+
+// -----------------------------State #'s-----------------------------------------
+
+
+
+
+
+
 
